f56072e1db6d0de67ef98d87a18d62c1603601e2
Brian McCarty <bkmccarty@gmail.com>
2019-01-23 11:57:44 -0800

    base module updates to export all functions

--- ITGlueAPI/ITGlueAPI.psm1
+++ ITGlueAPI/ITGlueAPI.psm1
@@ -9,6 +9,21 @@ $ITGlue_Headers.Add("Content-Type", 'application/vnd.api+json')
 
 Set-Variable -Name "ITGlue_Headers"  -Value $ITGlue_Headers -Scope global
 
+#Get public and private function definition files.
+$Public = @(Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -Recurse -Exclude *.tests.ps1, *profile.ps1 -ErrorAction SilentlyContinue)
+$Private = @(Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -Recurse -Exclude *.tests.ps1, *profile.ps1 -ErrorAction SilentlyContinue)
+#Dot source the files
+foreach ($import in @($Public + $Private)) {
+    try {
+        . $import.fullname
+    }
+    catch {
+        Write-Error -Message "Failed to import function $($import.fullname): $_"
+    }
+}
+# Export all the functions
+Export-ModuleMember -Function $Public.Basename -Alias *
+
 Import-ITGlueModuleSettings
 
 Set-ITGlueModuleAliases
