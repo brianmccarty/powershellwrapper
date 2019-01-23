e16840da4aa2edea151fffe271b155bfb9872c37
Brian McCarty <bkmccarty@gmail.com>
2019-01-23 11:51:25 -0800

    Update ITGlueAPI.psm1

--- ITGlueAPI/ITGlueAPI.psm1
+++ ITGlueAPI/ITGlueAPI.psm1
@@ -1,6 +1,14 @@
-$ITGlue_Headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"   
-$ITGlue_Headers.Add("Content-Type", 'application/vnd.api+json') 
+# Locations of settings path and file
+$ITGlueAPIConfPath = "$($env:PROGRAMDATA)\ITGlue"
+
+$ITGlueAPIConfFile = "config.psd1"
+
+$ITGlue_Headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+
+$ITGlue_Headers.Add("Content-Type", 'application/vnd.api+json')
 
 Set-Variable -Name "ITGlue_Headers"  -Value $ITGlue_Headers -Scope global
 
 Import-ITGlueModuleSettings
+
+Set-ITGlueModuleAliases
